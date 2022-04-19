e6d29288923cd63630d423bb77ba8ab6d7f552a4
Loes Reyniers <490511@student.fontys.nl>
2022-04-19 10:55:51 +0200

    Circustrein2 form aangemaakt.

---
+++ Circustrein/Circustrein2.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Circustrein
+{
+    partial class Circustrein2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Circustrein_gb = new System.Windows.Forms.GroupBox();
+            this.Enter_btn = new System.Windows.Forms.Button();
+            this.sVlees_lbl = new System.Windows.Forms.Label();
+            this.mVlees_lbl = new System.Windows.Forms.Label();
+            this.lVlees_lbl = new System.Windows.Forms.Label();
+            this.sPlant_lbl = new System.Windows.Forms.Label();
+            this.mPlant_lbl = new System.Windows.Forms.Label();
+            this.lPlant_lbl = new System.Windows.Forms.Label();
+            this.sVlees_txtb = new System.Windows.Forms.TextBox();
+            this.mVlees_txtb = new System.Windows.Forms.TextBox();
+            this.lVlees_txtb = new System.Windows.Forms.TextBox();
+            this.sPlant_txtb = new System.Windows.Forms.TextBox();
+            this.mPlant_txtb = new System.Windows.Forms.TextBox();
+            this.lPlant_txtb = new System.Windows.Forms.TextBox();
+            this.Circustrein_gb.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Circustrein_gb
+            // 
+            this.Circustrein_gb.Controls.Add(this.lPlant_txtb);
+            this.Circustrein_gb.Controls.Add(this.mPlant_txtb);
+            this.Circustrein_gb.Controls.Add(this.sPlant_txtb);
+            this.Circustrein_gb.Controls.Add(this.lVlees_txtb);
+            this.Circustrein_gb.Controls.Add(this.mVlees_txtb);
+            this.Circustrein_gb.Controls.Add(this.sVlees_txtb);
+            this.Circustrein_gb.Controls.Add(this.lPlant_lbl);
+            this.Circustrein_gb.Controls.Add(this.mPlant_lbl);
+            this.Circustrein_gb.Controls.Add(this.sPlant_lbl);
+            this.Circustrein_gb.Controls.Add(this.lVlees_lbl);
+            this.Circustrein_gb.Controls.Add(this.mVlees_lbl);
+            this.Circustrein_gb.Controls.Add(this.sVlees_lbl);
+            this.Circustrein_gb.Location = new System.Drawing.Point(40, 12);
+            this.Circustrein_gb.Name = "Circustrein_gb";
+            this.Circustrein_gb.Size = new System.Drawing.Size(578, 415);
+            this.Circustrein_gb.TabIndex = 0;
+            this.Circustrein_gb.TabStop = false;
+            this.Circustrein_gb.Text = "Circustrein";
+            // 
+            // Enter_btn
+            // 
+            this.Enter_btn.Location = new System.Drawing.Point(661, 366);
+            this.Enter_btn.Name = "Enter_btn";
+            this.Enter_btn.Size = new System.Drawing.Size(93, 45);
+            this.Enter_btn.TabIndex = 1;
+            this.Enter_btn.Text = "Enter";
+            this.Enter_btn.UseVisualStyleBackColor = true;
+            this.Enter_btn.Click += new System.EventHandler(this.Enter_btn_Click);
+            // 
+            // sVlees_lbl
+            // 
+            this.sVlees_lbl.AutoSize = true;
+            this.sVlees_lbl.Location = new System.Drawing.Point(32, 45);
+            this.sVlees_lbl.Name = "sVlees_lbl";
+            this.sVlees_lbl.Size = new System.Drawing.Size(120, 17);
+            this.sVlees_lbl.TabIndex = 0;
+            this.sVlees_lbl.Text = "Kleine vleeseters:";
+            this.sVlees_lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // mVlees_lbl
+            // 
+            this.mVlees_lbl.AutoSize = true;
+            this.mVlees_lbl.Location = new System.Drawing.Point(32, 74);
+            this.mVlees_lbl.Name = "mVlees_lbl";
+            this.mVlees_lbl.Size = new System.Drawing.Size(122, 17);
+            this.mVlees_lbl.TabIndex = 1;
+            this.mVlees_lbl.Text = "Middel vleeseters:";
+            // 
+            // lVlees_lbl
+            // 
+            this.lVlees_lbl.AutoSize = true;
+            this.lVlees_lbl.Location = new System.Drawing.Point(32, 106);
+            this.lVlees_lbl.Name = "lVlees_lbl";
+            this.lVlees_lbl.Size = new System.Drawing.Size(117, 17);
+            this.lVlees_lbl.TabIndex = 2;
+            this.lVlees_lbl.Text = "Grote vleeseters:";
+            // 
+            // sPlant_lbl
+            // 
+            this.sPlant_lbl.AutoSize = true;
+            this.sPlant_lbl.Location = new System.Drawing.Point(32, 165);
+            this.sPlant_lbl.Name = "sPlant_lbl";
+            this.sPlant_lbl.Size = new System.Drawing.Size(134, 17);
+            this.sPlant_lbl.TabIndex = 3;
+            this.sPlant_lbl.Text = "Kleine planteneters:";
+            // 
+            // mPlant_lbl
+            // 
+            this.mPlant_lbl.AutoSize = true;
+            this.mPlant_lbl.Location = new System.Drawing.Point(32, 194);
+            this.mPlant_lbl.Name = "mPlant_lbl";
+            this.mPlant_lbl.Size = new System.Drawing.Size(136, 17);
+            this.mPlant_lbl.TabIndex = 4;
+            this.mPlant_lbl.Text = "Middel planteneters:";
+            // 
+            // lPlant_lbl
+            // 
+            this.lPlant_lbl.AutoSize = true;
+            this.lPlant_lbl.Location = new System.Drawing.Point(32, 221);
+            this.lPlant_lbl.Name = "lPlant_lbl";
+            this.lPlant_lbl.Size = new System.Drawing.Size(131, 17);
+            this.lPlant_lbl.TabIndex = 5;
+            this.lPlant_lbl.Text = "Grote planteneters:";
+            // 
+            // sVlees_txtb
+            // 
+            this.sVlees_txtb.Location = new System.Drawing.Point(172, 45);
+            this.sVlees_txtb.Name = "sVlees_txtb";
+            this.sVlees_txtb.Size = new System.Drawing.Size(100, 22);
+            this.sVlees_txtb.TabIndex = 6;
+            this.sVlees_txtb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // mVlees_txtb
+            // 
+            this.mVlees_txtb.Location = new System.Drawing.Point(172, 73);
+            this.mVlees_txtb.Name = "mVlees_txtb";
+            this.mVlees_txtb.Size = new System.Drawing.Size(100, 22);
+            this.mVlees_txtb.TabIndex = 7;
+            // 
+            // lVlees_txtb
+            // 
+            this.lVlees_txtb.Location = new System.Drawing.Point(172, 101);
+            this.lVlees_txtb.Name = "lVlees_txtb";
+            this.lVlees_txtb.Size = new System.Drawing.Size(100, 22);
+            this.lVlees_txtb.TabIndex = 8;
+            // 
+            // sPlant_txtb
+            // 
+            this.sPlant_txtb.Location = new System.Drawing.Point(172, 165);
+            this.sPlant_txtb.Name = "sPlant_txtb";
+            this.sPlant_txtb.Size = new System.Drawing.Size(100, 22);
+            this.sPlant_txtb.TabIndex = 9;
+            // 
+            // mPlant_txtb
+            // 
+            this.mPlant_txtb.Location = new System.Drawing.Point(172, 191);
+            this.mPlant_txtb.Name = "mPlant_txtb";
+            this.mPlant_txtb.Size = new System.Drawing.Size(100, 22);
+            this.mPlant_txtb.TabIndex = 10;
+            // 
+            // lPlant_txtb
+            // 
+            this.lPlant_txtb.Location = new System.Drawing.Point(172, 216);
+            this.lPlant_txtb.Name = "lPlant_txtb";
+            this.lPlant_txtb.Size = new System.Drawing.Size(100, 22);
+            this.lPlant_txtb.TabIndex = 11;
+            // 
+            // Circustrein2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Enter_btn);
+            this.Controls.Add(this.Circustrein_gb);
+            this.Name = "Circustrein2";
+            this.Text = "Circustrein2";
+            this.Circustrein_gb.ResumeLayout(false);
+            this.Circustrein_gb.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Circustrein_gb;
+        private System.Windows.Forms.TextBox lPlant_txtb;
+        private System.Windows.Forms.TextBox mPlant_txtb;
+        private System.Windows.Forms.TextBox sPlant_txtb;
+        private System.Windows.Forms.TextBox lVlees_txtb;
+        private System.Windows.Forms.TextBox mVlees_txtb;
+        private System.Windows.Forms.TextBox sVlees_txtb;
+        private System.Windows.Forms.Label lPlant_lbl;
+        private System.Windows.Forms.Label mPlant_lbl;
+        private System.Windows.Forms.Label sPlant_lbl;
+        private System.Windows.Forms.Label lVlees_lbl;
+        private System.Windows.Forms.Label mVlees_lbl;
+        private System.Windows.Forms.Label sVlees_lbl;
+        private System.Windows.Forms.Button Enter_btn;
+    }
+}
---
+++ Circustrein/Circustrein2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circustrein
+{
+    public partial class Circustrein2 : Form
+    {
+        public Circustrein2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Enter_btn_Click(object sender, EventArgs e)
+        {
+            int sV = Convert.ToInt32(sVlees_txtb.Text);
+            int mV = Convert.ToInt32(mVlees_txtb.Text);
+            int lV = Convert.ToInt32(lVlees_txtb.Text);
+            int sP = Convert.ToInt32(sPlant_txtb.Text);
+            int mP = Convert.ToInt32(mPlant_txtb.Text);
+            int lP = Convert.ToInt32(lPlant_txtb.Text);
+        }
+    }
+}
